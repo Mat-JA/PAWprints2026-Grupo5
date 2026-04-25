825fdc5fea7d10d8443b444b20b6e0b8a48aad59
Nicolas Huici <111755754+nicodigo@users.noreply.github.com>
2026-04-25 20:06:56 -0300

    Refactorizo el router, en este momento está sincronizado con la primer serie de videos el proyecto

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -8,22 +8,45 @@ use App\Core\Exceptions\RouteNotFoundException;
 
 class Router
 {
-    public array $routes;
+    public array $routes = [
+        "GET" => [],
+        "POST" => [],
+    ];
 
-    public function dispatch($path): void
+    public function dispatch($path, $http_method = "GET"): void
     {
 
-        if (!array_key_exists($path, $this->routes)) {
+        if (!$this->routeExists($path, $http_method)) {
             throw new RouteNotFoundException("No existe ruta para este Path");
         }
 
-        list($controllerName, $method) = explode('@', $this->routes[$path]);
+        list($controllerName, $method) = $this->getController($path, $http_method);
         $controller = new ("App\\Controllers\\{$controllerName}");
         $controller->$method();
     }
 
-    public function  loadRoutes($path, $action): void
+    public function getController($path, $http_method)
     {
-        $this->routes[$path] = $action;
+        return explode('@', $this->routes[$http_method][$path]);
+    }
+
+    public function loadRoutes($path, $action, $http_method = "GET"): void
+    {
+        $this->routes[$http_method][$path] = $action;
+    }
+
+    public function get($path, $action)
+    {
+        $this->loadRoutes($path, $action, "GET");
+    }
+
+    public function post($path, $action)
+    {
+        $this->loadRoutes($path, $action, "POST");
+    }
+
+    public function routeExists($path, $http_method): bool
+    {
+        return (array_key_exists($path, $this->routes[$http_method]));
     }
 }
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -14,11 +14,10 @@ $log_app->pushHandler(new StreamHandler(__DIR__ . '/../storage/logs/app.log', Lo
 
 $router = new Router($log_app);
 
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$router->loadRoutes('/', 'PageController@home');
-$router->loadRoutes('/catalogo', 'PageController@catalogo');
-$router->loadRoutes('/eventos', 'PageController@eventos');
-$router->loadRoutes('/nosotros', 'PageController@acercaDeNosotros');
+$router->get('/', 'PageController@home');
+$router->get('/catalogo', 'PageController@catalogo');
+$router->get('/eventos', 'PageController@eventos');
+$router->get('/nosotros', 'PageController@acercaDeNosotros');
 
-$router->loadRoutes('not_found', 'ErrorController@notFound');
-$router->loadRoutes('internal_error', 'ErrorController@internalError');
+$router->get('not_found', 'ErrorController@notFound');
+$router->get('internal_error', 'ErrorController@internalError');
