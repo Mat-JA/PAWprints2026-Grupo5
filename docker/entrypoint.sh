dcb1f6b40f64bfc26a5d36d966895b35005fa940
Nicolas Huici <111755754+nicodigo@users.noreply.github.com>
2026-05-03 23:32:31 -0300

    agrego migraciones a entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 775 /var/www/html/storage
+vendor/bin/phinx migrate
+vendor/bin/phinx seed:run -s FirstTablesSeeder
 exec apache2-foreground
