3676cf88623867c7ae00f056ae895c7abba3735b
MateoAusqui <matuemc2@gmail.com>
2026-06-08 11:36:30 -0300

    mejora del punto 8

--- src/Core/TwigFactory.php
+++ src/Core/TwigFactory.php
@@ -12,8 +12,25 @@ class TwigFactory
         $loader = new FilesystemLoader(__DIR__ . '/../../views');
 
         $twig = new Environment($loader, [
-            'cache' => false, // cambiar a __DIR__ . '/../../storage/cache/twig' en producción
+            'cache'       => false,
             'auto_escape' => 'html',
+        ]);
+
+        $twig->addGlobal('store', [
+            'name'        => 'PAWPrints',
+            'url'         => 'http://localhost:8080',
+            'description' => 'Librería y espacio de lectura en Luján, Buenos Aires.',
+            'telephone'   => '+54-000-0000000',
+            'email'       => 'contacto@pawprints.com',
+            'foundingDate'=> '2024',
+            'address'     => [
+                'street'   => 'Calle Ejemplo 1234',
+                'locality' => 'Luján',
+                'region'   => 'Buenos Aires',
+                'postal'   => '6700',
+                'country'  => 'AR',
+            ],
+            'hours' => 'Mo-Sa 09:00-20:00',
         ]);
 
         return $twig;
